b4efb274f88a427b4df21644ad0b81c12434ec44
Daniel Williams <williamsoligodaniel@gmail.com>
2022-08-12 05:46:45 +0100

    added version

---
+++ Managing_Packages_with_NPM/version.js
@@ -0,0 +1,16 @@
+/*Add a Version to Your package.json
+A version is one of the required fields of your package.json file. This field describes the current version of your project. Here's an example:
+
+"version": "1.2.0",
+Add a version to the package.json file of your project. */
+
+{
+ "author": "Daniel Williams",
+ "description": "A project that shows package.json",
+ "keywords": [ "learn", "freecodecamp", "packages" ],
+ "license": "MIT",
+ "version": "1.2.0",
+ "dependencies": {
+  "express": "^4.18.1"
+ }
+}
