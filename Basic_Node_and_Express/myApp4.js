e8196d4cc29c397e719c15c57ad2e3829a6bd0b7
Daniel Williams <williamsoligodaniel@gmail.com>
2022-08-23 14:16:48 +0100

    Update and rename app4.js to myApp4.js

--- Basic_Node_and_Express/myApp4.js
+++ Basic_Node_and_Express/myApp4.js
@@ -1,4 +1,4 @@
-Serve Static Assets
+/*Serve Static Assets
 
 An HTML server usually has one or more directories that are accessible by the user. 
 You can place there the static assets needed by your application (stylesheets, scripts, images).
@@ -15,6 +15,7 @@ Mount the express.static() middleware to the path /public with app.use(). The ab
 
 Now your app should be able to serve a CSS stylesheet. Note that the /public/style.css 
 file is referenced in the /views/index.html in the project boilerplate. Your front-page should look a little better now!
+*/
 
 let express = require('express');
 let app = express("Hello World");
